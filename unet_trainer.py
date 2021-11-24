e2344d8a18c273aa1708c8287250b8921c402872
wcm23 <wcm23@cam.ac.uk>
2021-11-24 11:56:31 +0000

    Third Commit

--- unet_trainer.py
+++ unet_trainer.py
@@ -17,13 +17,10 @@ import os
 from tqdm import tqdm
 
 labels = 4
-trained = False
 path = 'C:\\Users\\mcgoug01\\OneDrive - CRUK Cambridge Institute\\Python Scripts\\kits21\\kits21\\data'
-kits = kpt.KiTS21_Data(path,n=5,num_class=labels)
+kits = kpt.KiTS21_Data(path,n=10,num_class=labels)
 kitsloader = DataLoader(dataset=kits,batch_size=2,shuffle=True)
 model_loc = os.path.join(os.getcwd(),'unet')
-print("")
-print("")
 
 if os.path.exists(model_loc): 
     model = torch.load(model_loc)
@@ -34,25 +31,29 @@ costs=[]
 opt = optim.Adam(model.parameters(),lr=0.0001)
 #this line taken from https://stackoverflow.com/questions/49201236/check-the-total-number-of-parameters-in-a-pytorch-model
 pytorch_total_params = sum(p.numel() for p in model.parameters())
-#scaling factor for loss is number of model parameters*number of input data
-scaler = pytorch_total_params*512*512
 print("Total number of model parameters (in millions):",pytorch_total_params/1e6)
 print("")
 loss= nn.CrossEntropyLoss()
 start = time.time()
 epochs = 8
 model.train()
-for epoch in range(1):
-    bar = tqdm(kitsloader)
-    for idx,(x,y) in enumerate(bar):
-        bar.set_description("Training from KiTS")
+for epoch in range(3):
+    # bar = tqdm(kitsloader)
+    for x,y in kitsloader:
+        # bar.set_description("Training from KiTS")
         x = x.reshape(x.shape[0],x.shape[1],x.shape[2],-1)
+        y = torch.transpose(y,1,3)
         pred = model(x.float())
         output = loss(pred, y)
         opt.zero_grad()
         output.backward()
         opt.step()
-        costs.append(float(output/scaler))
+        costs.append(float(output/(512*512*4)))
+        print(output/(512*512*4))
     torch.save(model,model_loc)
         
 plt.plot(costs)
+# count_dict ={}
+# for name, param in model.named_parameters():
+#     if 'weight' in name:
+#         count_dict[name] = param
